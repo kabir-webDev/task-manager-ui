eb7fb6de7bb56ff39db3b356eb4e51d100b8d130
kabir-webDev <kabir.webdev@gmail.com>
2024-01-17 13:11:48 +0600

    Adding another Stage

--- src/app/homepage/task-list/task-list.component.ts
+++ src/app/homepage/task-list/task-list.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class TaskListComponent {
   todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
+  inProgress = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
   done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
   drop(event: CdkDragDrop<string[]>) {
@@ -19,5 +19,8 @@ export class TaskListComponent {
     }
     console.log('Todo List:', this.todo);
     console.log('Done List:', this.done);
+  }
+  openTask(param:any) {
+    console.log('Open task',param);
   }
 }
